d1ea20ec3ee58025524d9112e4ec0effae99d77b
tomiwadotJS <victorgbayedev@gmail.com>
2023-10-25 22:38:15 +0100

    colourmill clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
 
 import './App.css'
+import { feedData } from './components/IGFeed/FeedData'
 import DiscoverCollections from './components/discoverCollections/DiscoverCollections'
 import Navigation from './components/navigation/Navigation'
 import Tab from './components/tabs/Tab'
+
 // import HeroImage from './assets/images/Colour_Mill_Web_Banner.png'; // Import the image file
 function App() {
   // const [count, setCount] = useState(0)
@@ -75,6 +77,82 @@ function App() {
       <p className="subSection3headerText">DISCOVER OUR COLLECTIONS</p>
         <DiscoverCollections/>
       </section>
+      {/* <section className='subSection4'>
+      <p>Subsection4</p>
+      </section> */}
+      <section className='subSection5'>
+        <header className='subSection5Header'>
+          <h2>Fill your feeds with colour</h2>
+        <div className='subSection5HeaderSocials'>
+          <span>
+            <p>Instagram</p>
+          </span>
+          <span>
+            <p>Youtube</p>
+          </span>
+        </div>
+        </header>
+        <main className='subSection5HeaderSocialsImage'>
+          {feedData.map((item)=>{
+            return(
+              <div key={item.id} className='subSection5HeaderSocialsImageItem'>
+                <img src={item.img}/>
+              </div>
+            )
+          })}
+        </main>
+      </section>
+      <section className='subSection7'>
+       <div className='subSection7SignupSec'>
+        <h2>Sign up to stay inspired</h2>
+        <div className='subSection7SignupSecInput'>
+          <input
+          placeholder='Email'
+          />
+        </div>
+        <p>We promise not to spam you</p>
+       </div>
+       <div className='subSection7ULContainer'>
+        <ul className='subSection7UL'>
+          <li>SHOP</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+        </ul>
+        <ul className='subSection7UL'>
+        <li>SHOP</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+        </ul>
+        <ul className='subSection7UL'>
+          <li>SHOP</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+          <li>Oil Blends</li>
+        </ul>
+       </div>
+      </section>
+      <div className="color-text">
+        <span>colour mill</span>
+        {/* <span>c</span>
+        <span>o</span>
+        <span>l</span>
+        <span>o</span>
+        <span>u</span>
+        <span>r</span>
+        <span> </span>
+        <span>m</span>
+        <span>i</span>
+        <span>l</span>
+        <span>l</span> */}
+</div>
     </>
   )
 }
---
+++ src/components/IGFeed/FeedData.js
@@ -0,0 +1,34 @@
+import feed1 from '../../assets/images/feed1.jpeg'
+import feed2 from '../../assets/images/feed2.jpeg'
+import feed3 from '../../assets/images/feed3.jpeg'
+import feed4 from '../../assets/images/feed4.jpeg'
+import feed5 from '../../assets/images/feed5.jpeg'
+import feed6 from '../../assets/images/feed6.jpeg'
+
+
+export const feedData = [
+    {
+        id: 1,
+        img: feed1
+    },
+    {
+        id: 2,
+        img: feed2
+    },
+    {
+        id: 3,
+        img: feed3
+    },
+    {
+        id: 4,
+        img: feed4
+    },
+    {
+        id: 5,
+        img: feed5
+    },
+    {
+        id: 6,
+        img: feed6
+    },
+]
